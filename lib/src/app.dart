5c24e7ed5bb5d53d32020fb9e226f9a30995c0d0
champoyradoo <champoyradoo@gmail.com>
2020-11-19 18:58:15 +0800

    Make app a stateful widget which records how many clicks a user does

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Let\'s see some images new structure!'),
         ),
+        body: Center(
+          child: Text('You have pressed the button $counter times')
+        ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('hi there!');
+            setState(() {
+              counter++;
+            });
           },
         ),
       ),
