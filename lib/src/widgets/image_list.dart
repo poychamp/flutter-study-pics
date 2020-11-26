8909e45b59e3a55426f79535e77a9e2a581eee00
champoyradoo <champoyradoo@gmail.com>
2020-11-26 17:38:45 +0800

    show a border on the image

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -17,7 +17,11 @@ class ImageList extends StatelessWidget {
 
   Widget buildImageBuilder(ImageModel image) {
     return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+      ),
       margin: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(20.0),
       child: Image.network(image.url),
     );
   }
